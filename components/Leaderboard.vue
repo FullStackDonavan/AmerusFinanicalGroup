025aecf9613974eaf7f6a5949458dc3cf8fdc113
DonavanJones <donavancjones@gmail.com>
2024-05-21 17:47:54 -0400

    updated Insurance sales reporting

--- components/Leaderboard.vue
+++ components/Leaderboard.vue
@@ -25,6 +25,44 @@ function search() {
 
 <template>
   <BasicSection class="mx-4">
+    <div>
+      <div class="flex m-5">
+        <label
+          for="search-dropdown"
+          class="mb-2 text-sm font-medium text-slate-900 sr-only dark:text-gray-300"
+          >Search Questions</label
+        >
+        <div class="relative w-full">
+          <input
+            v-model="searchInput"
+            @input="search"
+            type="search"
+            id="search-dropdown"
+            class="block p-2.5 w-full z-20 text-sm text-slate-900 bg-slate-50 rounded-lg border-solid border-2 border-indigo-600 dark:bg-gray-900 dark:text-gray-300"
+            placeholder="Search Questions..."
+          />
+          <button
+            type="submit"
+            class="absolute top-0 right-0 p-2.5 text-sm font-medium dark:text-white"
+          >
+            <svg
+              class="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
     <table class="w-full table-fixed border-collapse border border-gray-400">
       <thead>
         <tr class="bg-blue-500 text-white">
