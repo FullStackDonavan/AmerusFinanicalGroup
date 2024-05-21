b2296f6962c05ebd068a45a48c26a0f2100d01ce
DonavanJones <donavancjones@gmail.com>
2024-05-21 09:56:43 -0400

    updated Insurance sales reporting

--- components/elements/InsuranceSalesForm.vue
+++ components/elements/InsuranceSalesForm.vue
@@ -133,6 +133,23 @@ async function postInsuranceSales() {
 
     <div class="mb-6">
       <label
+        for="category"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+      >
+        Category
+      </label>
+      <input
+        v-model="data.category"
+        type="text"
+        id="category"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+        placeholder="Category"
+        required
+      />
+    </div>
+
+    <div class="mb-6">
+      <label
         for="policyYear"
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
       >
