f7ee54ad6f39bb775706b14cd2804e1af4a8e730
DonavanJones <donavancjones@gmail.com>
2024-05-21 20:57:56 -0400

    updated Insurance sales reporting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
