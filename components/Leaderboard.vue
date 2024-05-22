163cde3adeb256f1a5edcbebc0881fd7bf104853
DonavanJones <donavancjones@gmail.com>
2024-05-21 23:11:28 -0400

    updated Insurance sales reporting

--- components/Leaderboard.vue
+++ components/Leaderboard.vue
@@ -36,7 +36,7 @@
 </template>
 
 <script setup>
-import { getUserById } from "~/server/database/repositories/userRespository";
+import { getUserById } from "/server/database/repositories/userRespository";
 
 const isLoading = ref(false);
 const insuranceSales = ref([]);
