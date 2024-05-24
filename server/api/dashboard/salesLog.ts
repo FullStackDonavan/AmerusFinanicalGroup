eb267315b5753f157c9a119cc1042c7560841f7d
DonavanJones <donavancjones@gmail.com>
2024-05-23 21:44:50 -0400

    updated sales log reporting

--- server/api/dashboard/salesLog.ts
+++ server/api/dashboard/salesLog.ts
@@ -1,9 +1,27 @@
 import prisma from '~/server/database/client';
+import { getUserBySessionToken } from '~/server/app/services/sessionService';
+import sendDefaultErrorResponse from "~/server/app/errors/responses/DefaultErrorsResponse";
 
 export default defineEventHandler(async (event) => {
   try {
-    // Fetch all insurance sales
-    const insuranceSales = await prisma.insuranceSales.findMany();
+    const authToken = event.headers.authorization || null;
+    
+    if (!authToken) {
+      return sendDefaultErrorResponse(event, 'Unauthorized', 403, 'Authentication token missing');
+    }
+
+    const user = await getUserBySessionToken(authToken);
+
+    if (!user) {
+      return sendDefaultErrorResponse(event, 'Unauthorized', 403, 'Invalid authentication token');
+    }
+
+    // Fetch insurance sales belonging to the logged-in user
+    const insuranceSales = await prisma.insuranceSales.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
 
     // Map the sales data to include the client name
     const salesWithClientNames = insuranceSales.map((sale) => ({
